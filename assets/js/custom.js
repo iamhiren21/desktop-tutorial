64c8667e18b67e02a073272f3d076908ceed5c48
Hiren Savaliya <hirensavaliya234@gmail.com>
2023-05-02 21:54:22 +0530

    new

---
+++ assets/js/custom.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function(){
+    $('.menu_toggle_btn').click(function(){
+        $('.header_wrapper').toggleClass('menu_open');
+        $('body').toggleClass('cm-overflow');
+    });
+
+});
+
+$('#project_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ["<img src='assets/images/right_arrow.svg'> ", "<img src='assets/images/left_arrow.svg'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+})
+
+$('#review_slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ["<img src='assets/images/right_arrow.svg'> ", "<img src='assets/images/left_arrow.svg'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        992:{
+            items:2
+        }
+    }
+})
+
+ // Back To Top Button 
+ $(window).scroll(function(){ 
+    if ($(this).scrollTop() > 100) { 
+        $('#scroll').fadeIn(); 
+    } else { 
+        $('#scroll').fadeOut(); 
+    } 
+}); 
+$('#scroll').click(function(){ 
+    $("html, body").animate({ scrollTop: 0 }, 600); 
+    return false; 
+}); 
+
+$(document).ready(function(){
+    /*--------- Wow Js ----------*/
+    
+    new WOW().init();
+})
+
+// Section Redirect
+$('.scrollTo').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
